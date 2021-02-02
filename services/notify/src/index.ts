7fa8e18834f430752c3f4e9686e1ea49c1818e39
Shaun Jacobsen <shaun@sjacobsen.com>
2021-02-02 19:54:03 +0100

    Finishes email notification service

---
+++ services/notify/src/index.ts
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+import { channel } from './config/queue';
+import { sendEmail } from './email';
+
+const QUEUE_NAME = 'new_properties';
+
+const decode = (messageContent: any) => {
+  return JSON.parse(messageContent);
+};
+
+channel().then((canal) => {
+  canal.consume(QUEUE_NAME, (msg) => {
+    // when message received, do this
+    const listing = decode(msg.content);
+    // send the email and acknowledge
+    sendEmail([listing]).then(() => {
+      canal.ack(msg);
+    });
+  });
+});
