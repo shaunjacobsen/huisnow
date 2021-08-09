5fdb463c6b05622660e50ad6cf32168f7f94a78b
Shaun Jacobsen <shaun@sjacobsen.com>
2021-08-09 19:39:41 +0200

    Adds log message

--- services/notify/src/index.ts
+++ services/notify/src/index.ts
@@ -9,16 +9,18 @@ const decode = (messageContent: any) => {
   return JSON.parse(messageContent);
 };
 
-channel().then((canal) => {
-  canal.consume(QUEUE_NAME, (msg) => {
-    // when message received, do this
-    const listing = decode(msg.content);
-    console.log('message received!', listing);
-    // send the email and acknowledge
-    sendEmail([listing])
-      .then(() => {
-        canal.ack(msg);
-      })
-      .catch((e) => console.error('error, cannot send', e));
-  });
-});
+channel()
+  .then((canal) => {
+    canal.consume(QUEUE_NAME, (msg) => {
+      // when message received, do this
+      const listing = decode(msg.content);
+      console.log('message received!', listing);
+      // send the email and acknowledge
+      sendEmail([listing])
+        .then(() => {
+          canal.ack(msg);
+        })
+        .catch((e) => console.error('error, cannot send', e));
+    });
+  })
+  .catch((e) => console.log('channel error', e));
